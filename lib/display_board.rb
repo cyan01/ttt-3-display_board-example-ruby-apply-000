fd32e7cda65e7035b2bb3faef002f8ccfaed1f11
Cyan Williams <cyan.williams16@myhunter.cuny.edu>
2020-05-01 20:12:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board = 
+def display_board 
+  puts "   |  {board[2]}"
+  puts seperator = "-----------"
+  puts "  {board[3]}  |  {board[4]}  |  {board[5]}"
+  puts seperator
+  puts "  {board[6]}  |  {board[7]}  |  {board[8]}"
+  puts "\n"
+end
